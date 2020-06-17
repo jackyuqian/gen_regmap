f5d10463ecd697feb21599e80ad7abe8e38e2658
qianqyu <qianqyu@tencent.com>
2020-06-17 17:14:23 +0800

    updage

--- gen.py
+++ gen.py
@@ -1,32 +1,75 @@
 #!/usr/bin/python3
 import csv, sys, getopt
 
-def read_csv(fcsv):
+def read_csv(fcsv, delimiter=','):
     with open(fcsv, 'r', encoding='UTF-8-sig') as fp:
         ## Read CSV
-        dict_csv    = csv.DictReader(fp)
+        #### Pre-Process
+        lines   = []
+        for line in fp:
+            line.strip()
+            if line.startswith('#'):
+                continue
+            elif line.replace(delimiter, '').strip() == '':
+                continue
+            lines.append(line)
+
+        #### Read CSV
+        dict_csv    = csv.DictReader(lines)
         
         ## Build Data Structure
-        ##  Regmap  = [ 
+        ##  regmap  = [ 
         ##      {
         ##          'Name'      : 'xxx',    (str)
-        ##          'Address'   : xxx,      (int) 
-        ##          'Field'     : {
+        ##          'Address'   : 'xxx',    (str) 
+        ##          'Field'     : [{
         ##                      'Name'      : 'xxx',    (str)
         ##                      'Msb'       : xxx,      (int) 
         ##                      'Lsb'       : xxx,      (int)
         ##                      'Length'    : xxx,      (int)
         ##                      'Access'    : 'xxx',    (str)
-        ##                      'Reset'     : xxx       (int) 
-        ##                      }
+        ##                      'Reset'     : 'xxx',    (str) 
+        ##                      'Doc'       : 'xxx'     (str) 
+        ##                      }, {...}, ...]
         ##      },
         ##      {...},
         ##      ...
         ##      {...}
         ##  ]
         ## 
+        regmap  = []
+
         for row in dict_csv:
-            print(row['Address'])
+            print(row['Field'])
+            if row['Address'].strip() != '':
+                bits        = row['Bits'].strip().replace('[','').replace(']','').split(':')
+                register    = {
+                        'Name'      : (row['Register'].strip() if row['Register'].strip() != '' else 'reg_' + row['Address'].strip()),
+                        'Address'   : row['Address'].strip(),
+                        'Field'     : [{
+                            'Name'  : row['Field'].strip(),
+                            'Msb'   : int(bits[0]),
+                            'Lsb'   : int(bits[1]),
+                            'Length': (int(bits[0]) - int(bits[1]) + 1),
+                            'Access': row['Access'].strip(),
+                            'Reset' : row['Reset'].strip(),
+                            'Doc'   : row['Doc']
+                            }]
+                        }
+                regmap.append(register)
+            else:
+                field   = {
+                        'Name'  : row['Field'].strip(),
+                        'Msb'   : int(bits[0]),
+                        'Lsb'   : int(bits[1]),
+                        'Length': (int(bits[0]) - int(bits[1]) + 1),
+                        'Access': row['Access'].strip(),
+                        'Reset' : row['Reset'].strip(),
+                        'Doc'   : row['Doc']
+                        }
+                regmap[-1]['Field'].append(field)
+        return regmap
+
 
 def print_help():
     print('Oh!')
@@ -51,7 +94,8 @@ def main(argv):
             frtl    = arg
     
     ## Main Flow
-    read_csv(fcsv)
+    regmap  = read_csv(fcsv)
+    print(regmap)
 
 if __name__ == "__main__":
     main(sys.argv[1:])
