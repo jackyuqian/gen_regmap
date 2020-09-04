79cc10e16eff72ef08207b985a6a482ed54a542c
qianqyu <qianqyu@tencent.com>
2020-09-04 15:03:00 +0800

    add gen verification

--- csv2rtl.py
+++ csv2rtl.py
@@ -2,20 +2,22 @@
 import  sys, getopt
 from    parse_csv   import *
 from    gen_rtl     import *
+from    gen_ver     import *
 
 def print_usage():
-    print('./csv2rtl.py -i <csv file> -o <rtl file> -b <delimiter> -d <data_bw> -a <addr_bw>')
+    print('./csv2rtl.py -i <csv file> -r <rtl file> -v <ver file> -b <delimiter> -d <data_bw> -a <addr_bw>')
 
 def main(argv):
     # Get Arguments
     fcsv        = ''
     frtl        = ''
+    fver        = ''
     data_bw     = 32
     addr_bw     = 12
     delimiter   = ','
 
     try:
-        opts, args    = getopt.getopt(argv,"hi:o:d:a:b:")
+        opts, args    = getopt.getopt(argv,"hi:r:v:d:a:b:")
     except getopt.GetoptError:
         print_usage()
         sys.exit(2)
@@ -25,8 +27,10 @@ def main(argv):
             sys.exit()
         elif opt in ("-i"):
             fcsv       = arg
-        elif opt in ("-o"):
+        elif opt in ("-r"):
             frtl       = arg
+        elif opt in ("-v"):
+            fver       = arg
         elif opt in ("-d"):
             data_bw     = int(arg)
         elif opt in ("-a"):
@@ -38,6 +42,8 @@ def main(argv):
         fcsv    = 'default.csv'
     if frtl == '':
         frtl    = fcsv.split('.')[0] + '.v'
+    if fver == '':
+        fver    = fcsv.split('.')[0] + '_ver.v'
     module_name = fcsv.split('.')[0]
 
     ## Main Flow
@@ -45,9 +51,13 @@ def main(argv):
         regmap  = parse_csv(fp, data_bw, delimiter)
     
     rtl_txt = gen_rtl(regmap, module_name, data_bw, addr_bw)
+    ver_txt = gen_ver(regmap, module_name, data_bw, addr_bw)
     
     with open(frtl, 'w') as fp:
         print(rtl_txt, file=fp)
+    
+    with open(fver, 'w') as fp:
+        print(ver_txt, file=fp)
 
 if __name__ == "__main__":
     main(sys.argv[1:])
---
+++ gen_ver.py
@@ -0,0 +1,84 @@
+#!/usr/bin/python3 -B
+import json, sys, getopt
+
+def gen_ver(regmap, module_name, data_bw, addr_bw):
+    txt =  ""
+    txt += "bit [%d :0] rdata;\n" % (data_bw - 1)
+    txt += "bit         fail;\n"
+    txt += "fail    = 1'b0;\n"
+    txt =  "\n"
+
+    ## Read Logic
+    for register in regmap:
+        txt += "    // %s\n" % (register['Name'])
+        txt += "    if_apb_reg.read(%d'h%x, rdata);\n" % (addr_bw, register['Address'])
+        idx_bit = data_bw - 1
+        txt_dv  = "{" # Default Value
+        while idx_bit >= 0:
+            field_found = False
+            for field in register['Field']:
+                if idx_bit <= field['Msb'] and idx_bit >= field['Lsb']:
+                    # regname =register['Name'] + '_' + field['Name']
+                    regname = field['Name'] + ('' if register['GroupIdx'] == -1 else ('[' + str(register['GroupIdx']) + ']')) 
+                    if field['Name'] in ['RSVD', 'Reserved']:
+                        txt_dv += "%d'h0," % field['Length']
+                    else:
+                        txt_dv += field['Reset'] + ","
+                    idx_bit = field['Lsb'] - 1
+                    field_found = True
+            if field_found is False:
+                txt_dv += "1'b0,"
+                idx_bit = idx_bit - 1
+        txt_dv =    txt_dv[:-1] + '}'
+        txt += "    if(rdata != %s) begin;\n" % txt_dv
+        txt += "        fail    = 1'b1;\n"
+        txt += '        $display("[ERROR] Addr = 0x%x, Rdata = ' %(register['Address']) + '0x%x", rdata);\n'
+        txt += "    end;\n"
+    txt += "\n"
+
+    return txt
+
+def print_usage():
+    print('./gen_rtl.py -i <json file> -o <rtl file> -d <data_bw> -a <addr_bw>')
+    
+def main(argv):
+    # Get Arguments
+    fjson       = ''
+    frtl        = ''
+    data_bw     = 32
+    addr_bw     = 12
+
+    try:
+        opts, args    = getopt.getopt(argv,"hi:o:d:a:")
+    except getopt.GetoptError:
+        print_usage()
+        sys.exit(2)
+    for opt, arg in opts:
+        if opt == '-h':
+            print_usage()
+            sys.exit()
+        elif opt in ("-i"):
+            fjson       = arg
+        elif opt in ("-o"):
+            frtl       = arg
+        elif opt in ("-d"):
+            data_bw     = int(arg)
+        elif opt in ("-a"):
+            addr_bw     = int(arg)
+    
+    if fjson == '':
+        fjson   = 'default.json'
+    if frtl == '':
+        frtl    = fjson.split('.')[0] + '.v'
+    module_name = fjson.split('.')[0]
+
+    ## Main Flow
+    with open(fjson, 'r') as fp:
+        regmap  = json.load(fp)
+    rtl_txt = gen_rtl(regmap, module_name, data_bw, addr_bw)
+    with open(frtl, 'w') as fp:
+        print(rtl_txt, file=fp)
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
+
